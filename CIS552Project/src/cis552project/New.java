a33475f6a88a03a7530ee409385d4373874b588b
mandoor <mandoor@umassd.edu>
2020-02-13 18:32:00 -0500

    New Changes

--- CIS552Project/src/cis552project/New.java
+++ CIS552Project/src/cis552project/New.java
@@ -13,6 +13,8 @@ public class New {
    
    public static void main(String[] args) {
         // TODO code application logic here
+       
         System.out.println("Hello");
+        System.out.println("Database Design");
     }
 }

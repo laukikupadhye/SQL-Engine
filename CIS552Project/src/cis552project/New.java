7e2b8c30de96f054afdc08d29ef996f393bd16ab
Pardeep Pathania <pardeeppathania09@gmail.com>
2020-02-13 18:38:49 -0500

    Test Commit

--- CIS552Project/src/cis552project/New.java
+++ CIS552Project/src/cis552project/New.java
@@ -14,5 +14,6 @@ public class New {
    public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello");
+        System.out.println("Database Design");
     }
 }

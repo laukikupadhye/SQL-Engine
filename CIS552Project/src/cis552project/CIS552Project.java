81ca92830910e658306808603f6d1d7558a035f4
Pardeep Pathania <pardeeppathania09@gmail.com>
2020-02-14 17:49:20 -0500

    Parser verification.

--- CIS552Project/src/cis552project/CIS552Project.java
+++ CIS552Project/src/cis552project/CIS552Project.java
@@ -16,6 +16,7 @@ import java.util.logging.Logger;
 import net.sf.jsqlparser.parser.CCJSqlParser;
 import net.sf.jsqlparser.parser.ParseException;
 import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.create.table.ColumnDefinition;
 import net.sf.jsqlparser.statement.create.table.CreateTable;
 import net.sf.jsqlparser.statement.select.FromItem;
 import net.sf.jsqlparser.statement.select.Join;
@@ -36,7 +37,8 @@ public class CIS552Project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Reader reader = new StringReader("Select R.* FROM R , S");
+        //Reader reader = new StringReader("Select R.* FROM R , S");
+        Reader reader = new StringReader("CREATE TABLE R(A int, B int)");
         CCJSqlParser parser = new CCJSqlParser(reader);
         try {
             Statement statement = parser.Statement();
@@ -54,6 +56,10 @@ public class CIS552Project {
                     System.out.println(joins);
                     fileRead();
                 }
+            }else if(statement instanceof CreateTable){
+                CreateTable createTable= (CreateTable) statement;
+                List<ColumnDefinition> columnDefinitions = createTable.getColumnDefinitions();
+                System.out.println(columnDefinitions);
             }
         } catch (ParseException ex) {
             Logger.getLogger(CIS552Project.class.getName()).log(Level.SEVERE, null, ex);
@@ -67,11 +73,11 @@ public class CIS552Project {
         Scanner myReader = new Scanner(myObj);
         while (myReader.hasNextLine()) {
             String data = myReader.nextLine();
-            String[] split = data.split("|");
+            String[] split = data.split("\\|");
             for (String item:split){
                 System.out.println(item);
             }
-            System.out.println(data + " ---> " +split.length + " --- " + split[0] + "-" + split[1] + "-" + split[2]);
+            System.out.println(data + " ---> " +split.length + " --- " + split[0] + "-" + split[1]);
       }
       myReader.close();
 
